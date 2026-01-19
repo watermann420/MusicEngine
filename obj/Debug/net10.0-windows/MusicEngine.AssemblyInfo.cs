2332326adcb70a53fab5ca2f69c63d8bd9518566
watermann420 <watermannog@gmail.com>
2026-01-19 09:34:34 +0100

    Test Script

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd99537c986a1a512c0109101de5e1bad780db3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b66aab8d60d6b24f048eadca9fd2d54ae1a1a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
