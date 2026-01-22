de1e05317e61041fbb023aa5d1d9f3c7de7a1cf1
nullonebinary <bytezer0ffensive@protonmail.com>
2026-01-22 13:31:09 +0100

    -Added safe mode to adress and fix memory leak

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e7fa0e94aa872176b1f9c9374551475c6aca60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef3a1df887c5ff904509174bab3bc5cfaf2c63bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
