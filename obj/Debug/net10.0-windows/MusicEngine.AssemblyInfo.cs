ed486e71a72c0d94700ece3338041a8fcfbef812
watermann420 <watermannog@gmail.com>
2026-01-26 17:59:05 +0100

    License (MEL)

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b19d3a3ff5ebb40fb43efc53af351c55b722c4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3682dbcaa022fd41e80afa42fe0e944bdcc39ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
