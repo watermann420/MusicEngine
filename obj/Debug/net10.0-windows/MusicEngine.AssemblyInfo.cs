b7721f7aa85ca5bb53abddea7565c14494a2e40f
nullonebinary <bytezer0ffensive@protonmail.com>
2026-02-02 06:50:02 +0100

    --

--- obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/MusicEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13dafd4cfd99851d2eaf2fb45cd8738624e03bfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7c3cd5ca7c7e403354c1671a6e48ad9f3db61c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
